d6b4e6c009a2be2396172599f09253c3b87f73cd
nishant <nishant.singh87@gmail.com>
2016-09-18 14:04:10 +0530

    more example for equal

--- spec/look_like_spec.rb
+++ spec/look_like_spec.rb
@@ -7,5 +7,9 @@ describe "look_like" do
 
   it "every thing looks like itself" do
     expect("Sam").to look_like("Sam")
+    expect("1").to look_like("1")
+    expect("<:L#$%WRGSDF").to look_like("<:L#$%WRGSDF")
+    expect("true").to look_like("true")
   end
+
 end
