1895a27f70196a81a4ea5d3a347e04e5950a40b3
nishant <nishant.singh87@gmail.com>
2016-09-18 20:18:03 +0530

    refactoring.

--- lib/matchers/not-found.rb
+++ lib/matchers/not-found.rb
@@ -1,12 +1,9 @@
-LookLike::Matchers.define({
-                              :name     => :not_found,
-                              :desc     => "no matcher defined",
-                              :priority => 10,
-                              :select   => lambda{|keyword|
-                                true
-                              },
-                              :match    => lambda{|actual|
-                                false
-                              }
-                          })
+LookLike::Matchers.define(
+    {
+        :name     => :not_found,
+        :desc     => "no matcher defined",
+        :priority => 10,
+        :select   => lambda { |keyword| true },
+        :match    => lambda { |actual| false }
+    })
 
--- lib/matchers/one_word.rb
+++ lib/matchers/one_word.rb
@@ -1,12 +1,9 @@
-LookLike::Matchers.define({
-                              :name     => :one_word,
-                              :desc     => "one word",
-                              :priority => 7,
-                              :select   => lambda{|keyword|
-                                LookLike::MatcherSupport.count_words(keyword) == 1
-                              },
-                              :match    => lambda{|actual|
-                                LookLike::MatcherSupport.count_words(actual) == 1
-                              }
-                          })
+LookLike::Matchers.define(
+    {
+        :name     => :one_word,
+        :desc     => "one word",
+        :priority => 7,
+        :select   => lambda{|keyword| LookLike::MatcherSupport.count_words(keyword) == 1 },
+        :match    => lambda{|actual| LookLike::MatcherSupport.count_words(actual) == 1 }
+    })
 
