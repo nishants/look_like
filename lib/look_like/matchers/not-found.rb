21bc3ea7a1150f67bbddabf5a779e0d8df22fd20
nishant <nishant.singh87@gmail.com>
2016-10-03 15:26:41 +0530

    rearranging.

--- lib/look_like/matchers/not-found.rb
+++
@@ -1,8 +0,0 @@
-LookLike::Matchers.define(
-    {
-        :name     => :not_found,
-        :desc     => "no matcher defined",
-        :select   => lambda { |keyword| true },
-        :match    => lambda { |actual| false }
-    })
-
