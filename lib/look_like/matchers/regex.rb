eda0660a9d0a12e601d2042aa60c2d839d20ca82
nishant <nishant.singh87@gmail.com>
2016-10-08 22:31:23 +0530

    fix regular expressions.

--- lib/look_like/matchers/regex.rb
+++ lib/look_like/matchers/regex.rb
@@ -1,12 +1,12 @@
 LookLike::Matchers.define(
     {
         :name     => :regex,
-        :desc     => "regex ",
+        :desc     => "regex",
         :select   => lambda{|expected|
           expected.start_with?("/") && expected.end_with?("/")
         },
         :match    => lambda{|actual, expected|
-          Regexp.new(expected.tr("/", "").strip) === actual
+          /#{Regexp.new(expected.tr("/", "").strip)}/ === actual
         }
     })
 
--- spec/lib/matchers/regex_spec.rb
+++ spec/lib/matchers/regex_spec.rb
@@ -2,6 +2,9 @@ require "spec_helper"
 
 describe "regex matching" do
   it "should support regex" do
+    expect("1-12-1-2").to look_like("/[\\d\\-]+/")
+    expect("$3.67").to look_like("/\\$(?<dollars>\\d+)\\.(?<cents>\\d+)/")
+    expect("$367").not_to look_like("/\\$(?<dollars>\\d+)\\.(?<cents>\\d+)/")
     expect("i have  test").to look_like("/test/")
     expect("i have  tess").not_to look_like("/test/")
   end
